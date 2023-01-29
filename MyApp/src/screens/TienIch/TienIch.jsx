1f42dc7d5926394a8c5f89749a7d19e4ee99a314
UIT19522473 <71434447+UIT19522473@users.noreply.github.com>
2023-01-29 15:16:15 +0700

    commit
    commit

--- MyApp/src/screens/TienIch/TienIch.jsx
+++ MyApp/src/screens/TienIch/TienIch.jsx
@@ -4,8 +4,54 @@ import Icon from 'react-native-vector-icons/MaterialIcons';
 import {AuthContext} from '../../components/context';
 import {useContext} from 'react';
 
+import {writeFile, readFile, DownloadDirectoryPath} from 'react-native-fs';
+import XLSX from 'xlsx';
+
 const TienIch = ({navigation}) => {
   const {signOutMain} = useContext(AuthContext);
+
+  const exportDataToExcel = () => {
+    console.log('xuat excel');
+    var data = [
+      {name: 'John', city: 'Seattle'},
+      {name: 'Mike', city: 'Los Angeles'},
+      {name: 'Zach', city: 'New York'},
+    ];
+
+    var ws = XLSX.utils.json_to_sheet(data);
+
+    var wb = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Users');
+
+    const wbout = XLSX.write(wb, {type: 'binary', bookType: 'xlsx'});
+    // var RNFS = require('react-native-fs');
+    // var file = RNFS.ExternalStorageDirectoryPath + '/test.xlsx';
+    // writeFile(file, wbout, 'ascii')
+    //   .then(r => {
+    //     alert('export thanh cong');
+    //   })
+    //   .catch(e => {
+    //     alert('export that bai', e);
+    //   });
+
+    writeFile(DownloadDirectoryPath + '/sample.csv', wbout, 'ascii')
+      .then(r => {
+        alert(DownloadDirectoryPath + '/ajootec.csv');
+      })
+      .catch(e => {
+        alert(e);
+      });
+
+    // XLSX.utils.book_append_sheet(wb, ws, 'demoExcel', true);
+
+    // const base64 = XLSX.write(wb, {type: 'base64'});
+    // const filename = FileSystem.documentDirectory + 'demoExcel.xlsx';
+    // FileSystem.writeAsStringAsunc(filename, base64, {
+    //   encoding: FileSystem.EncodingType.Base64,
+    // }).then(() => {
+    //   Sharing.shareAsync(filename);
+    // });
+  };
   return (
     <View className="flex-1">
       <View className="w-full justify-center items-center my-2 border-b-[0.7px] p-4 mb-8">
@@ -22,7 +68,9 @@ const TienIch = ({navigation}) => {
             Hạn mức chi
           </Text>
         </TouchableOpacity>
-        <TouchableOpacity className="w-[150px] h-[150px] border-[1px] rounded-3xl border-green-500 justify-center items-center">
+        <TouchableOpacity
+          onPress={() => exportDataToExcel()}
+          className="w-[150px] h-[150px] border-[1px] rounded-3xl border-green-500 justify-center items-center">
           <Icon size={46} name="file-present" color="green" />
           <Text className="mt-4 text-base font-semibold text-gray-500">
             Xuất file excel

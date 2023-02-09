a2f400791ddaf00e2f64173073dcee2daae99038
UIT19522473 <71434447+UIT19522473@users.noreply.github.com>
2023-02-09 09:49:02 +0700

    commit

--- MyApp/src/screens/TienIch/Currency.jsx
+++ MyApp/src/screens/TienIch/Currency.jsx
@@ -1,76 +1,104 @@
-import { Button, StyleSheet, Text, View, SafeAreaView, ActivityIndicator } from 'react-native'
-import React from 'react'
-import MoneyHeader from './CurrencyComponent/MoneyHeader'
-import { TextInput } from 'react-native-element-textinput'
-import Icon from 'react-native-vector-icons/MaterialIcons'
-import InternationalView from './CurrencyComponent/InternationalView'
-import { getAPIKeyRate } from './CurrencyComponent/getExchangeRate'
-import { FlatList, ScrollView } from 'react-native-gesture-handler'
-import { useState, useEffect } from 'react'
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  ActivityIndicator,
+  TouchableOpacity,
+} from 'react-native';
+import React from 'react';
+import MoneyHeader from './CurrencyComponent/MoneyHeader';
+import {TextInput} from 'react-native-element-textinput';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import InternationalView from './CurrencyComponent/InternationalView';
+import {getAPIKeyRate} from './CurrencyComponent/getExchangeRate';
+import {FlatList, ScrollView} from 'react-native-gesture-handler';
+import {useState, useEffect} from 'react';
 
-
-const Currency = ({ route }) => {
-
-
+const Currency = ({route, navigation}) => {
   const [currencyList, setCurrencyList] = useState([]);
   const [money, setMoney] = useState(1);
-  const [searchKey, setSearchKey] = useState("");
+  const [searchKey, setSearchKey] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const handleMoneyChange = updatedMoney => {
     setMoney(updatedMoney);
   };
   const base = route.params?.base || 'USD';
-  const baseSymbol = route.params?.symbol || '$'
+  const baseSymbol = route.params?.symbol || '$';
 
   useEffect(() => {
     setIsLoading(true);
     getAPIKeyRate(base).then(result => {
       setCurrencyList(result);
       setIsLoading(false);
-
     });
-
   }, [base]);
 
   currencyList.forEach(el => {
     el.amount = (money * el.rate).toLocaleString('fr-FR');
-  })
+  });
   console.log(money.toLocaleString());
 
   const filterCurrencyList = currencyList.filter(item => {
-    return item.fullName.toLowerCase().includes(searchKey.toLowerCase()) ||
-      item.name.toLowerCase().includes(searchKey.toLowerCase());
+    return (
+      item.fullName.toLowerCase().includes(searchKey.toLowerCase()) ||
+      item.name.toLowerCase().includes(searchKey.toLowerCase())
+    );
   });
 
-if (isLoading)
+  if (isLoading)
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size="large" />
+        <Text>Đang tải dữ liệu</Text>
+      </View>
+    );
+
   return (
-    <View style={styles.loadingContainer}>
-      <ActivityIndicator size="large" />
-      <Text>Đang tải dữ liệu</Text>
-    </View>
-  );
-
-return (
-  <SafeAreaView style={styles.container}>
-    <MoneyHeader onMoneyChange={handleMoneyChange} base={base} value={money} symbol={baseSymbol} />
-    <View style={styles.searchBarContainer}>
-      <Icon name='search' size={24} />
-      <TextInput style={styles.searchInput} placeholder={"Tìm theo mã, tên hoặc ký hiệu"}
-        onChangeText={setSearchKey}
-        value={searchKey}
+    <SafeAreaView style={styles.container}>
+      <View className="flex-row items-center bg-primary p-2 py-4">
+        <TouchableOpacity onPress={() => navigation.navigate('TienIchHome')}>
+          <Icon name="arrow-back" size={32} color="white" />
+        </TouchableOpacity>
+        <View className="items-center w-[90%]">
+          <Text className="text-gray-50 font-bold text-2xl">
+            Tra cứu tỷ giá
+          </Text>
+        </View>
+      </View>
+      <MoneyHeader
+        onMoneyChange={handleMoneyChange}
+        base={base}
+        value={money}
+        symbol={baseSymbol}
       />
-    </View>
-    <ScrollView>
-      {filterCurrencyList.map(item => (
-        <InternationalView unit={item.name} unitLogo={item.symbol} uriFlag={item.uriFlag} currentRate={item.rate} pickedUnit={base} amount={item.amount} />
-      ))}
-    </ScrollView>
+      <View style={styles.searchBarContainer}>
+        <Icon name="search" size={24} />
+        <TextInput
+          style={styles.searchInput}
+          placeholder={'Tìm theo mã, tên hoặc ký hiệu'}
+          onChangeText={setSearchKey}
+          value={searchKey}
+        />
+      </View>
+      <ScrollView>
+        {filterCurrencyList.map(item => (
+          <InternationalView
+            unit={item.name}
+            unitLogo={item.symbol}
+            uriFlag={item.uriFlag}
+            currentRate={item.rate}
+            pickedUnit={base}
+            amount={item.amount}
+          />
+        ))}
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
-  </SafeAreaView>
-)
-}
-
-export default Currency
+export default Currency;
 
 const styles = StyleSheet.create({
   container: {
@@ -85,8 +113,7 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     marginVertical: 10,
     paddingHorizontal: 10,
-    borderRadius: 10
-
+    borderRadius: 10,
   },
   searchInput: {
     width: '90%',
@@ -101,4 +128,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     flex: 1,
   },
-})
+});
--- MyApp/src/screens/TienIch/XuatFile/XuatFile.jsx
+++ MyApp/src/screens/TienIch/XuatFile/XuatFile.jsx
@@ -1,8 +1,12 @@
 import {View, Text, TouchableOpacity} from 'react-native';
 import React from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import {useSelector, useDispatch} from 'react-redux';
 
 const XuatFile = ({navigation}) => {
+  const dataAll = useSelector(State => State.dataAll);
+  const auth = useSelector(State => State.auth);
+
   return (
     <View className="flex-1">
       <View className="flex-row items-center bg-primary p-2 py-4">
@@ -12,6 +16,19 @@ const XuatFile = ({navigation}) => {
         <View className="w-[85%] items-center">
           <Text className="text-gray-50 font-bold text-2xl">Xuất file</Text>
         </View>
+      </View>
+
+      <Text className="border-b-2">{auth.userName}</Text>
+      <View className="border-b-2">
+        {dataAll.arrItem.map(item => (
+          <Text>{item.name}</Text>
+        ))}
+      </View>
+
+      <View className="">
+        {dataAll.arrTrans.map(trans => (
+          <Text>{trans.id}</Text>
+        ))}
       </View>
     </View>
   );
